b89da5fcd84fb09ac2d3200cf16614228fb4dd0c
Caio Lucas <caiolucas0430@gmail.com>
2024-10-26 14:38:20 -0300

    transerindo para o .c e .h

--- main.c
+++ main.c
@@ -104,68 +104,6 @@ void exibir_menu(void) {
     } while (escolha != 0);
 }
 
-void cadastro_pessoal(void) {
-    int escolha;
-    char id[20];  
-    char nome[50];
-    char cpf[15];
-    char data_nascimento[11]; 
-
-    printf("/////////////////////////////////////////////////////////////////////\n");
-    printf("///          = = = = = = = Cadastro Pessoa = = = = = = =          ///\n");
-    printf("/////////////////////////////////////////////////////////////////////\n");
-
-    printf("#### Selecione uma das opções: \n");
-    printf("1. Cadastrar Pessoa\n");
-    printf("2. Pesquisar Pessoa\n");
-    printf("3. Editar Pessoa   \n");
-    printf("4. Excluir Pessoa  \n");
-    printf("5. Sair\n");
-    scanf("%d", &escolha);
-    getchar();  
-
-    switch (escolha) {
-        case 1:
-            printf("Digite o ID: ");
-            fgets(id, 20, stdin);
-            id[strcspn(id, "\n")] = '\0';  
-
-            printf("Digite o Nome completo: ");
-            fgets(nome, 50, stdin);
-            nome[strcspn(nome, "\n")] = '\0';  
-
-            printf("Digite o CPF: ");
-            fgets(cpf, 15, stdin);
-            cpf[strcspn(cpf, "\n")] = '\0';  
-
-            printf("Digite a Data de Nascimento (dd/mm/aaaa): ");
-            fgets(data_nascimento, 11, stdin);
-            data_nascimento[strcspn(data_nascimento, "\n")] = '\0';  
-
-            printf("\nCadastro realizado com sucesso!\n");
-            printf("ID: %s\n", id);
-            printf("Nome: %s\n", nome);
-            printf("CPF: %s\n", cpf);
-            printf("Data de Nascimento: %s\n", data_nascimento);
-            break;
-
-        case 2:
-            printf("Pesquisar Pessoa.\n");
-            break;
-        
-        case 3:
-            printf("Editar Pessoa.\n");
-            break;
-        
-        case 4:
-    
-
-        default:
-            printf("Opção inválida! Por favor, tente novamente.\n");
-            break;
-    }
-}
-
 void produtos(void) {
     int escolha;
     char id_codigo[20];      
