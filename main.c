f9f50ad7b5c85470572b8f587ac6b99ecbacc470
Caio Lucas <caiolucas0430@gmail.com>
2024-10-29 21:50:46 -0300

    att do main

--- main.c
+++ main.c
@@ -9,102 +9,83 @@
 ///                                Semana 4                               ///
 ///////////////////////////////////////////////////////////////////////////////
 
-#include <locale.h>
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "pessoa.h"  
 #include "orcamento.h"
 #include "relatorio.h"
 #include "produtos.h"
 
 
-void exibir_menu(void);
-void produtos(void);
-void controle_estoque(void);
-void equipe(void);
+//////
+////// Assinaturas das funções
+//////
+
+char exibir_menu(void);
+void tela_sobre(void);
+void tela_equipe(void);
+
+//////
+////// Programa principal
+//////
 
 int main(void) {
-    setlocale(LC_ALL, "Portuguese");
-    printf("Bem vindo ao seu controle de despesas\n");   
-    exibir_menu();    
+    char escolha;
+
+    do {
+        escolha = exibir_menu();
+        switch(escolha) {
+            case '1':   menu_cadastro_pessoal();
+                        break;
+            case '2':   menu_produtos();
+                        break;
+            case '3':   menu_cadastro_orcamento();
+                        break;  
+            case '4':   menu_relatorios();
+                        break;
+            case '5':   tela_sobre();
+                        tela_equipe();
+                        break;
+        } 	
+    } while (escolha != '0');
+
     return 0;
 }
 
+//////
+////// Funções do Módulo Principal
+//////
 
 
-void exibir_menu(void) {
-    int escolha;
-    do {
-        printf("/////////////////////////////////////////////////////////////////////\n");
-        printf("///   Escolha um módulo:                                             ///\n");
-        printf("///   1. Cadastro Pessoal                                            ///\n");
-        printf("///   2. Produtos                                                    ///\n");
-        printf("///   3. Orçamento                                                   ///\n");
-        printf("///   4. Relatorios                                                  ///\n");
-        printf("///   5. Equipe                                                      ///\n");
-        printf("///   0. Sair                                                        ///\n");
-        printf("/////////////////////////////////////////////////////////////////////\n");
+char exibir_menu(void) {
+    char escolha;
+
+
+
+         system("clear||cls");
+        printf("\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("///   Escolha um módulo:                                                    ///\n");
+        printf("///   1. Cadastro Pessoal                                                   ///\n");
+        printf("///   2. Produtos                                                           ///\n");
+        printf("///   3. Orçamento                                                          ///\n");
+        printf("///   4. Relatorios                                                         ///\n");
+        printf("///   5. Equipe                                                             ///\n");
+        printf("///   0. Sair                                                               ///\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
         printf("#### Selecione uma das opções: \n");
-        scanf("%d", &escolha);
-        getchar();  
-        
-        switch (escolha) {
-            case 1:
-                cadastrar_pessoa();
-                break;
-            case 2:
-                produtos();
-                break;
-            case 3:
-                menu_cadastro_orcamento();
-                break;
-            case 4:
-                menu_relatorios();
-                break;
-            case 5:
-                equipe();
-                break;
-            case 0:
-                printf("Saindo do sistema...\n");
-                break;
-            default:
-                printf("Opção inválida! Tente novamente.\n");
-                break;
-        }
-    } while (escolha != 0);
-}
-
-void equipe(void) {
-    int escolha;
-
-    printf("\n");
-    printf("/////////////////////////////////////////////////////////////////////\n");
-    printf("///             Universidade Federal do Rio Grande do Norte         ///\n");
-    printf("///                 Centro de Ensino Superior do Seridó             ///\n");
-    printf("///               Departamento de Computação e Tecnologia           ///\n");
-    printf("///                  Disciplina DCT1106 -- Programação              ///\n");
-    printf("///                  Projeto Sistema de Despensa Doméstica          ///\n");
-    printf("///          Developed by @caiolucas_lm & @lelebiglove              ///\n");
-    printf("/////////////////////////////////////////////////////////////////////\n");
-    printf("///            Caio Lucas Lopes Medeiros                            ///\n");
-    printf("///            Leandro Isaac Correia de Brito                       ///\n");
-    printf("/////////////////////////////////////////////////////////////////////\n");
-    printf("\n");
-    printf("#### Selecione uma das opções: \n");
-    printf("1. Voltar ao menu principal\n");
-    printf("2. Sair\n");
-    scanf("%d", &escolha);
-    getchar();  
-
-    switch (escolha) {
-        case 1:
-            printf("Voltando ao menu principal...\n");
-            break;
-        case 2:
-            printf("Saindo do programa...\n");
-            break;
-        default:
-            printf("Opção inválida! Por favor, tente novamente.\n");
-            break;
+        scanf("%c", &escolha);    
+        getchar();
+        printf("///                                                                         ///\n");
+        printf("///                                                                         ///\n");
+        printf("///////////////////////////////////////////////////////////////////////////////\n");
+        printf("\n");
+        printf("\t\t\t<<< ... Aguarde ... >>>\n");
+        sleep(1);
+        return escolha;
     }
-}
+    
+
+
